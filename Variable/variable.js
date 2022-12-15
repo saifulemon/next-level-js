03fae22b3f6c657a0060b0c5c0231c65c63aeed9
saifulemon <463saiful0@gmail.com>
2022-12-15 18:31:55 +0600

    Different aspects of variables

---
+++ Variable/variable.js
@@ -0,0 +1,13 @@
+// var is function scope
+var city = "Dhaka";
+console.log(city);
+
+// let is block scope and can`t re-declare same identifier
+// let variables can be reassigned but not redeclared
+let shohor = "Chattogram";
+console.log(shohor);
+
+// const variables can't reassigned & redeclared
+const country = "Noakhali";
+country = "United States";
+console.log(country);
